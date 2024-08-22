aa0916829be01b08bf1ea5c22f62f38e7a89a908
SteffenHakestad <Hakestad.steffen@gmail.com>
2024-08-22 16:57:38 +0200

    Finished media preview on home page + navigation

--- Frontend/src/components/MediaPostPreviewComponent.js
+++ Frontend/src/components/MediaPostPreviewComponent.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function MediaPostPreview({ MediaTitle, MediaText, ImagePath }) {
-  return (
-    <>
-      <div id="preview-media-container">
-        <img className="media-preview-image" alt="media" src={`${ImagePath}`} />
+	return (
+		<>
+			<div id="preview-media-container">
+				<img className="media-preview-image" alt="media" src={`${ImagePath}`} />
 
-        <h1 id="preview-title">{MediaTitle}</h1>
-        <p id="preview-text">{MediaText}</p>
-        <Link to="/media">
-          <button className="std-btn readmore-btn">Les mer..</button>
-        </Link>
-      </div>
-    </>
-  );
+				<h1 id="preview-title">{MediaTitle}</h1>
+				<p id="preview-text">{MediaText}</p>
+				<Link to="/media">
+					<button className="std-btn readmore-btn">Les mer..</button>
+				</Link>
+			</div>
+		</>
+	);
 }
--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -5,58 +5,97 @@ import MediaPostPreviewComponent from "../components/MediaPostPreviewComponent";
 import Footer from "../components/Footer";
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
-  const [currentPost, setCurrentPost] = useState(0);
+	const [posts, setPosts] = useState([]);
+	const [currentPost, setCurrentPost] = useState(0);
 
-  useEffect(() => {
-    // Fetch data from the backend
-    fetch("/api/posts")
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+	useEffect(() => {
+		// Fetch data from the backend
+		fetch("/api/posts")
+			.then((response) => response.json())
+			.then((data) => setPosts(data))
+			.catch((error) => console.error("Error fetching data:", error));
+	}, []);
 
-  const NextPost = () => {
-    setCurrentPost((prevPostNumber) => (prevPostNumber + 1) % posts.length);
-  };
-  return (
-    <>
-      <HeroImageComponent />
-      <ServicesComponent />
-      <div className="about-outer-container">
-        <div className="about-inner-container">
-          <div className="about-text-container">
-            <h1>Guri Brekke</h1>
-            <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              varius luctus dignissim. Mauris vehicula erat id orci faucibus, ac
-              porttitor mauris accumsan. Quisque tellus est, pellentesque ut
-              ornare quis, posuere ornare massa. Suspendisse quis orci
-              hendrerit."
-            </p>
-          </div>
-          <div className="about-image-container">
-            <div className="image-shell">
-              <img src="/assets/images/StockPhoto.jpg" alt="about me" />
-            </div>
-          </div>
-        </div>
-      </div>
+	//Function to cycle between post previews
+	const CyclePost = (direction) => {
+		if (posts.length === 0) return;
 
-      <div id="mediapost-preview-home-container">
-        <div className="h-media-divider"></div>
-        <h1 id="preview-header-text">Nyeste Media Innlegg:</h1>
-        <div id="mediapost-preview-home-inner-container">
-          {posts.length > 0 && (
-            <MediaPostPreviewComponent
-              key={posts[currentPost]._id}
-              MediaTitle={posts[currentPost].headline}
-              MediaText={posts[currentPost].mediaText}
-              ImagePath={`${posts[currentPost].image}`}
-            />
-          )}
+		if (direction === "next") {
+			setCurrentPost((currentPost) => (currentPost + 1) % posts.length);
+		} else if (direction === "prev") {
+			setCurrentPost(
+				(currentPost) => (currentPost - 1 + posts.length) % posts.length
+			);
+		} else {
+			console.log("Next or prev?????");
+		}
+	};
 
-          {/* {posts.length > 0 && (
+	return (
+		<>
+			<HeroImageComponent />
+			<ServicesComponent />
+			<div className="about-outer-container">
+				<div className="about-inner-container">
+					<div className="about-text-container">
+						<h1>Guri Brekke</h1>
+						<p>
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+							varius luctus dignissim. Mauris vehicula erat id orci faucibus, ac
+							porttitor mauris accumsan. Quisque tellus est, pellentesque ut
+							ornare quis, posuere ornare massa. Suspendisse quis orci
+							hendrerit."
+						</p>
+					</div>
+					<div className="about-image-container">
+						<div className="image-shell">
+							<img src="/assets/images/StockPhoto.jpg" alt="about me" />
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* Media post preview container */}
+			{posts.length > 0 && (
+				<div id="mediapost-preview-home-container">
+					<div className="h-media-divider"></div>
+					<h1 id="preview-header-text">Nyeste Media Innlegg:</h1>
+
+					<div id="mediapost-preview-home-inner-container">
+						<button
+							className="cycle-post-btn"
+							onClick={() => CyclePost("prev")}>
+							<img
+								className="cycle-post-svg"
+								alt="previous icon"
+								src="\assets\icons\icon-left-arrow.svg"></img>
+						</button>
+						<MediaPostPreviewComponent
+							key={posts[currentPost]._id}
+							MediaTitle={posts[currentPost].headline}
+							MediaText={posts[currentPost].mediaText}
+							ImagePath={`${posts[currentPost].image}`}
+						/>
+						<button
+							className="cycle-post-btn"
+							onClick={() => CyclePost("next")}>
+							<img
+								className="cycle-post-svg"
+								alt="next icon"
+								src="\assets\icons\icon-right-arrow.svg"></img>
+						</button>
+					</div>
+
+					<div className="h-media-divider-small"></div>
+				</div>
+			)}
+			<Footer />
+		</>
+	);
+}
+
+//For a preview of the 3 newest media posts.
+{
+	/* {posts.length > 0 && (
             <MediaPostPreviewComponent
               key={posts[0]._id}
               MediaTitle={posts[0].headline}
@@ -83,17 +122,7 @@ export default function Home() {
               MediaText={posts[2].mediaText}
               ImagePath={`${posts[2].image}`}
             />
-          )} */}
-        </div>
-        <button className="std-btn" onClick={() => NextPost()}>
-          NextPost
-        </button>
-
-        <div className="h-media-divider-small"></div>
-      </div>
-
-      <Footer />
-    </>
-  );
+          )} */
 }
+
 // background: url(http://localhost:3000/static/media/StockPhoto.9a78d0ada57723114d66.jpg) no-repeat;
